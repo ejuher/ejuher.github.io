395c4e9dd068b9aa7a0600ae85d2524eda03a367
Erich Uher <ejuher@gmail.com>
2014-11-28 23:46:54 -0800

    wrote data to submit with artist form ajax request

--- noiseviolator/js/views/artistForm.js
+++ noiseviolator/js/views/artistForm.js
@@ -1,8 +1,36 @@
 NoiseViolator.Views.ArtistForm = Backbone.View.extend({
 	template: _.template(
-		"<label for='cell-number'>Cellphone Number</label>" +
-		"<input id='cell-number' type='tel'><input type='button' value='Add Contact' class='button'>"
+		"<form>" +
+			"<label for='artist-name'>Artist Name</label>" +
+			"<input id='artist-name'><input type='submit' value='Find Artist' class='button'>" + 
+		"</form>"
+
 	),
+
+	events: {
+		'submit form': 'setArtist'
+	},
+
+	setArtist: function() {
+		event.preventDefault();
+		var artistName = this.$el.find('#artist-name').val();
+		this.$el.find('#artist-name').val('');
+		$.ajax({
+			url: 'http://ws.audioscrobbler.com/2.0/',
+			dataType: 'json',
+			data: {
+				method: 'artist.getinfo',
+				artist: artistName,
+				autocorrect: 1,
+				api_key: 'f33b1bdb4bead8c686120d81d2baa09b',
+				format: 'json'
+			},
+			success: function(data) {
+				console.log('success');
+			}
+		})
+		console.log(artistName);
+	},
 
 	render: function() {
 		this.$el.html(this.template);
